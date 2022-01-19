208cb364f43874e2f8e1a0e22abc767d911e0e06
vishaldutt457 <duttvishal689@gmail.com>
2022-01-19 23:18:24 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {Grid} from "@material-ui/core"
+import Details from './components/Details/Details';
+
+
+function App() {
+  return (
+    <div className="App">
+      hello
+     <Grid  container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+     <Grid item xs={12} sm={4} >
+          <Details title="Income"/>
+        </Grid>
+        <Grid item xs={12} sm={4} >
+         Main
+        </Grid>
+        <Grid item xs={12} sm={4} >
+          <Details title="Expense"/>
+        </Grid>
+      </Grid>
+   
+    </div>
+  );
+}
+
+export default App;
